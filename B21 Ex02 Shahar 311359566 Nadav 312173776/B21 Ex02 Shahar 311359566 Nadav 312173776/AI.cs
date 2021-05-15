9b18faea088f0eb6a2c04592cadb1db616dee7fd
Shavits <s.shavit4@gmail.com>
2021-05-15 12:12:04 +0300

    Update AI.cs

--- B21 Ex02 Shahar 311359566 Nadav 312173776/B21 Ex02 Shahar 311359566 Nadav 312173776/AI.cs
+++ B21 Ex02 Shahar 311359566 Nadav 312173776/B21 Ex02 Shahar 311359566 Nadav 312173776/AI.cs
@@ -11,16 +11,27 @@ namespace B21_Ex02_Shahar_311359566_Nadav_312173776
         private static Random rand = new Random();
         public static void MakeAIMove(BoardLogic io_Board)
         {
-            int row = rand.Next(0, io_Board.BoardSize);
-            int col = rand.Next(0, io_Board.BoardSize);
-            Move aiMove = new Move(row, col);
-            Thread.Sleep(1500);
-            while (!io_Board.UpdateBoard(aiMove))
+            Thread.Sleep(500);
+            List<Move> availableMoves = GetAvailableMoves(io_Board);
+            Move chosenMove = availableMoves[rand.Next(availableMoves.Count)];
+            io_Board.UpdateBoard(chosenMove);
+        }
+
+        private static List<Move> GetAvailableMoves(BoardLogic i_board)
+        {
+            List<Move> availableMoves = new List<Move>();
+            for (int i = 0; i < i_board.BoardSize; i++)
             {
-                row = rand.Next(0, io_Board.BoardSize);
-                col = rand.Next(0, io_Board.BoardSize);
-                aiMove = new Move(row, col);
+                for(int j = 0; j <i_board.BoardSize; j++)
+                {
+                    if(i_board.BoardMatrix[i,j] == Move.enumXO.EMPTY)
+                    {
+                        Move possibleMove = new Move(i, j);
+                        availableMoves.Add(possibleMove);
+                    }
+                }
             }
+            return availableMoves;
         }
 
     }
